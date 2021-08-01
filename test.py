5dd094874986294aeda0a1e416a022a410d68ffe
Lesmana Zimmer <lesmana@gmx.de>
2021-08-01 17:33:16 +0200

    add unittest

---
+++ test.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+# image to ansi blocks
+# Copyright Lesmana Zimmer lesmana@gmx.de
+# Licensed under WTFPL version 2
+# http://www.wtfpl.net/about/
+
+import unittest
+
+import imagetoansiblocks as t
+
+class TestPixelToAnsiBlock(unittest.TestCase):
+
+  def test_bothcolor(self):
+    upperpixel = (101, 102, 103, 255)
+    lowerpixel = (201, 202, 203, 255)
+    ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel)
+    self.assertEqual(ansiblock, '\033[38;2;101;102;103;48;2;201;202;203m\u2580\033[0m')
+
+if __name__ == '__main__':
+  unittest.main()
