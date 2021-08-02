985285e558e2a2aa66be46fd3fbfecb737fa354c
Lesmana Zimmer <lesmana@gmx.de>
2021-08-02 10:25:36 +0200

    use nicer literal values

--- test.py
+++ test.py
@@ -12,53 +12,53 @@ import imagetoansiblocks as t
 class TestPixelToAnsiBlock(unittest.TestCase):
 
   def test_bothalpha(self):
-    upperpixel = (101, 102, 103, 0)
-    lowerpixel = (201, 202, 203, 0)
+    upperpixel = (11, 22, 33, 0)
+    lowerpixel = (44, 55, 66, 0)
     alphathreshold = 128
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
     self.assertEqual(ansiblock, ' ')
 
   def test_upperalpha(self):
-    upperpixel = (101, 102, 103, 0)
-    lowerpixel = (201, 202, 203, 255)
+    upperpixel = (11, 22, 33, 0)
+    lowerpixel = (44, 55, 66, 255)
     alphathreshold = 128
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
-    self.assertEqual(ansiblock, '\033[38;2;201;202;203m\u2584\033[0m')
+    self.assertEqual(ansiblock, '\033[38;2;44;55;66m\u2584\033[0m')
 
   def test_loweralpha(self):
-    upperpixel = (101, 102, 103, 255)
-    lowerpixel = (201, 202, 203, 0)
+    upperpixel = (11, 22, 33, 255)
+    lowerpixel = (44, 55, 66, 0)
     alphathreshold = 128
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
-    self.assertEqual(ansiblock, '\033[38;2;101;102;103m\u2580\033[0m')
+    self.assertEqual(ansiblock, '\033[38;2;11;22;33m\u2580\033[0m')
 
   def test_bothcolor(self):
-    upperpixel = (101, 102, 103, 255)
-    lowerpixel = (201, 202, 203, 255)
+    upperpixel = (11, 22, 33, 255)
+    lowerpixel = (44, 55, 66, 255)
     alphathreshold = 128
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
-    self.assertEqual(ansiblock, '\033[38;2;101;102;103;48;2;201;202;203m\u2580\033[0m')
+    self.assertEqual(ansiblock, '\033[38;2;11;22;33;48;2;44;55;66m\u2580\033[0m')
 
   def test_alphathreshold0(self):
-    upperpixel = (101, 102, 103, 99)
-    lowerpixel = (201, 202, 203, 99)
+    upperpixel = (11, 22, 33, 99)
+    lowerpixel = (44, 55, 66, 99)
     alphathreshold = 100
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
     self.assertEqual(ansiblock, ' ')
 
   def test_alphathreshold1(self):
-    upperpixel = (101, 102, 103, 99)
-    lowerpixel = (201, 202, 203, 100)
+    upperpixel = (11, 22, 33, 99)
+    lowerpixel = (44, 55, 66, 100)
     alphathreshold = 100
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
-    self.assertEqual(ansiblock, '\033[38;2;201;202;203m\u2584\033[0m')
+    self.assertEqual(ansiblock, '\033[38;2;44;55;66m\u2584\033[0m')
 
   def test_alphathreshold2(self):
-    upperpixel = (101, 102, 103, 100)
-    lowerpixel = (201, 202, 203, 99)
+    upperpixel = (11, 22, 33, 100)
+    lowerpixel = (44, 55, 66, 99)
     alphathreshold = 100
     ansiblock = t.pixeltoansiblock(upperpixel, lowerpixel, alphathreshold)
-    self.assertEqual(ansiblock, '\033[38;2;101;102;103m\u2580\033[0m')
+    self.assertEqual(ansiblock, '\033[38;2;11;22;33m\u2580\033[0m')
 
 class TestPixelsToDoubleRows(unittest.TestCase):
 
